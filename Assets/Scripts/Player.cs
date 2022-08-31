382168fd275508610a86ad10dd5555746285bf5b
Marcelo <marcelo-bareisys@prodesan.com.br>
2022-08-31 15:27:42 -0300

    variables, escapsule, abstraction, etc

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    
+    private Rigidbody playerRb;
+    private Animator playerAnim;
+
+    private bool isOnGround = true;
+    public bool IsOnGround
+    {
+        get { return isOnGround; }
+        set { isOnGround = value; }
+    }
+
+    private float jumpForce = 3.5f;
+    public float JumpForce
+    {
+        get { return jumpForce; }
+        set { jumpForce = value; }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Jump();
     }
+
+
+
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && this.IsOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            this.IsOnGround = false;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            this.IsOnGround = true;
+        }
+    }
+
+
 }
